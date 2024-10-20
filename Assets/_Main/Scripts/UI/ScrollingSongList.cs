126c014e6a616ef5e0b3ce1813a03251bce56bd3
Sparkfire <48664233+Sparkfir3@users.noreply.github.com>
2024-10-20 11:42:39 -0500

    we have the INFINITY SCROLL

--- Assets/_Main/Scripts/UI/ScrollingSongList.cs
+++ Assets/_Main/Scripts/UI/ScrollingSongList.cs
@@ -1,24 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Sparkfire.Sample
 {
-    [RequireComponent(typeof(VerticalLayoutGroup))]
-    public class ScrollingSongList : MonoBehaviour, IDragHandler
+    [RequireComponent(typeof(ScrollRect))]
+    public class ScrollingSongList : MonoBehaviour
     {
         [Header("Runtime Data"), SerializeField]
-        private List<Transform> listItems;
-        [SerializeField]
-        private List<SongInfoDisplay> infoDisplays;
+        private List<RectTransform> listItems;
 
         [Header("Settings"), SerializeField]
         private float listEntryHeight = 100; // TODO - scale with screen size
         [SerializeField]
         private float xOffset;
+        [SerializeField]
+        private float maxHeightUp = 600;
+        [SerializeField]
+        private float maxHeightDown = 600;
         [SerializeField]
         private bool loopTop = true;
         [SerializeField]
@@ -27,44 +30,26 @@ namespace Sparkfire.Sample
         [Header("Object References"), SerializeField]
         private GameObject listEntryPrefab;
         [SerializeField]
-        private RectTransform rectTransform;
+        private ScrollRect scrollRect;
+
+        private RectTransform content => scrollRect.content;
+        private RectTransform viewport => scrollRect.viewport;
 
         // ------------------------------
 
         #region Unity Functions
 
-        private void Update()
+        private void Start()
         {
-            if(Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                LoopListDown();
-            }
-            else if(Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                LoopListUp();
-            }
-        }
-
-        private void LateUpdate()
-        {
-            if(loopBottom && rectTransform.anchoredPosition.y < -listEntryHeight)
-                LoopListUp();
-            if(loopTop && rectTransform.anchoredPosition.y > listEntryHeight)
-                LoopListDown();
-
-            UpdateListEntryXOffsets();
-        }
-
-        public void OnDrag(PointerEventData data)
-        {
-            rectTransform.anchoredPosition += new Vector2(0f, data.delta.y);
+            scrollRect.onValueChanged.AddListener(CheckForLooping);
+            ValidateListEntries();
         }
 
 #if UNITY_EDITOR
         private void OnValidate()
         {
-            if(!rectTransform)
-                rectTransform = GetComponent<RectTransform>();
+            if(!scrollRect)
+                scrollRect = GetComponent<ScrollRect>();
         }
 #endif
 
@@ -88,29 +73,54 @@ namespace Sparkfire.Sample
 
         public void UpdateListEntryXOffsets()
         {
-            foreach(SongInfoDisplay display in infoDisplays)
+            foreach(RectTransform item in listItems)
             {
-                float yOffset = display.transform.position.y - transform.parent.TransformPoint(Vector3.zero).y;
+                if(item.childCount == 0 || !item.GetChild(0).TryGetComponent(out RectTransform child))
+                    return;
+
+                float yOffset = item.transform.position.y - transform.parent.TransformPoint(Vector3.zero).y;
                 float yOffsetCount = yOffset / listEntryHeight;
-                display.rectTransform.anchoredPosition = new Vector2(yOffsetCount * xOffset, 0f);
+                child.anchoredPosition = new Vector2(yOffsetCount * xOffset, 0f);
             }
         }
 
-        // Potential for optimization - instead of rebuilding layout groups, manually place each element as we re-order them
-        private void LoopListUp()
+        private void CheckForLooping(Vector2 value)
         {
-            rectTransform.GetChild(transform.childCount - 1).SetAsFirstSibling();
-            rectTransform.anchoredPosition += Vector2.up * listEntryHeight;
-            LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
+            if(content.childCount == 0)
+                return;
+
+            // Check lower bound
+            Transform lastChild = content.GetChild(content.childCount - 1);
+            if(loopBottom && lastChild.transform.position.y - viewport.transform.position.y < -maxHeightDown)
+                LoopListBottom();
+            // Check lower bound
+            else if(loopTop && content.GetChild(0).transform.position.y - viewport.transform.position.y > maxHeightUp)
+                LoopListTop();
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(content);
             UpdateListEntryXOffsets();
         }
 
-        private void LoopListDown()
+        // Potential for optimization - instead of rebuilding layout groups, manually place each element as we re-order them
+        private void LoopListBottom()
         {
-            rectTransform.GetChild(0).SetAsLastSibling();
-            rectTransform.anchoredPosition -= Vector2.up * listEntryHeight;
-            LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
-            UpdateListEntryXOffsets();
+            content.GetChild(content.childCount - 1).SetAsFirstSibling();
+            content.anchoredPosition += Vector2.up * listEntryHeight;
+            UpdateScrollRectMouseStartPosition(-listEntryHeight);
+        }
+
+        private void LoopListTop()
+        {
+            content.GetChild(0).SetAsLastSibling();
+            content.anchoredPosition -= Vector2.up * listEntryHeight;
+            UpdateScrollRectMouseStartPosition(listEntryHeight);
+        }
+
+        private void UpdateScrollRectMouseStartPosition(float offset)
+        {
+            FieldInfo field = typeof(ScrollRect).GetField("m_PointerStartLocalCursor",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+            field.SetValue(scrollRect, (Vector2)field.GetValue(scrollRect) + Vector2.up * offset);
         }
 
         #endregion
@@ -121,22 +131,20 @@ namespace Sparkfire.Sample
 
         public void ValidateListEntries()
         {
-            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, 0f);
+            content.anchoredPosition = new Vector2(content.anchoredPosition.x, 0f);
 
             listItems.Clear();
-            foreach(Transform child in transform)
+            foreach(Transform child in content)
             {
                 if(child.TryGetComponent(out RectTransform rect))
                 {
-                    listItems.Add(child);
+                    listItems.Add(rect);
                     rect.sizeDelta = new Vector2(rect.sizeDelta.x, listEntryHeight);
                 }
             }
-            LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
-            infoDisplays = listItems.Select(x => x.GetComponentInChildren<SongInfoDisplay>()).ToList();
+            LayoutRebuilder.ForceRebuildLayoutImmediate(content);;
 
             UpdateListEntryXOffsets();
-            Debug.Assert(listItems.Count == infoDisplays.Count);
         }
 
         #endregion
